0b643d15b36e4b9c27000c3b9da4f3a47fad713c
kieuvanh1999 <01248991825lala@gmail.com>
2022-09-30 15:11:39 +0700

    js

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ $(window).scroll(function() {
         addClassHeader.removeClass('fixNav');
     }
 });
+
+function openCity(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(cityName).style.display = "block";
+  }
+
+  $('.construction-list').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    Infinity: false,
+    rows: 2,
+  });
+
+AOS.init({
+    duration: 600,
+});
